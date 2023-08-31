a0663a3ad107039a5a4316b76617b02afcb8321f
driftlog <driftlogged@gmail.com>
2023-08-31 09:04:01 +0800

    first commit

---
+++ src/main/java/edu/sg/nus/iss/csf_assessment/CsfAssessmentApplication.java
@@ -0,0 +1,13 @@
+package edu.sg.nus.iss.csf_assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsfAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsfAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/sg/nus/iss/csf_assessment/CsfAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sg.nus.iss.csf_assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsfAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
